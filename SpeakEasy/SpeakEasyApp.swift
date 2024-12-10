38df5b7fce04b14ce73a8b55a630ea5059f61403
Bastien BOMBARDELLA <bastien.bc@outlook.fr>
2024-12-10 09:11:58 +0100

    Initial commit

---
+++ SpeakEasy/SpeakEasyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpeakEasyApp.swift
+//  SpeakEasy
+//
+//  Created by Bastien Bombardella on 10/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct SpeakEasyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
