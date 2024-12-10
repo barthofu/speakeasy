f97f77698ff251eae72cb04649b986803d56327c
Florian HAVARD <florian.havard@gmail.com>
2024-12-10 15:01:12 +0100

    feat(Map) ajout Map + bouchon de donnnées de tests

--- SpeakEasy/Views/ContentView.swift
+++ SpeakEasy/Views/ContentView.swift
@@ -12,7 +12,7 @@ struct ContentView: View {
     
     var body: some View {
             TabView {
-                MapView().tabItem {
+                MapView(listLocations: Location.getLyonLocationsWithFullData()).tabItem {
                     Label("Map", systemImage: "map.fill")
                 }
                 
