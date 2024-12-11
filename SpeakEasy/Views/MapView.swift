04b6ed962c98cdcdf98bcadebdd1f06516907a25
Florian HAVARD <florian.havard@gmail.com>
2024-12-11 06:18:56 -0800

    feat(Geolocalisation): add Point + init map from current position

--- SpeakEasy/Views/MapView.swift
+++ SpeakEasy/Views/MapView.swift
@@ -11,26 +11,19 @@ import MapKit
 struct MapView: View {
     
     @State private var locationService = LocationService(completer: .init())
-    @State private var position = MapCameraPosition.region(
-        MKCoordinateRegion(
-            center: CLLocationCoordinate2D(latitude: 45.7640, longitude: 4.8357),
-            span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
-        )
-    )
+    @StateObject var manager = LocationManager()
     
     var listLocations: [LocationModel]
     
     var body: some View {
         NavigationStack {
             ZStack(alignment: .topTrailing) {
-                Map(position: $position) {
-                    ForEach(listLocations) { location in
-                        Annotation(location.name, coordinate: location.coordinates()) {
-                            NavigationLink {
-                                DetailView(location: location)
-                            } label: {
-                                VStack {
-                                    Image(systemName: location.type.icon)
+                Map(coordinateRegion: $manager.region, showsUserLocation: true, annotationItems: listLocations, annotationContent:
+                        {
+                    location in
+                        
+                    MapAnnotation(coordinate: location.coordinates()) {
+                                Image(systemName: location.type.icon)
                                         .foregroundStyle(.white)
                                         .padding(8)
                                         .background(Color("Primary"))
@@ -43,12 +36,9 @@ struct MapView: View {
                                         .padding(4)
                                         .background(Color.white.opacity(0.9))
                                         .cornerRadius(8)
-                                }
                             }
-                        }
-                        .annotationTitles(.hidden)
-                    }
-                }
+                            
+                })
                 .mapStyle(.imagery(elevation: .realistic))
                 
                 NavigationLink {
