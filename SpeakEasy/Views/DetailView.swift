7324e468d320568316b193ccab4f36e83d023afe
Bastien BOMBARDELLA <bastien.bc@outlook.fr>
2024-12-11 10:27:02 +0100

    feat: refacto view details

--- SpeakEasy/Views/DetailView.swift
+++ SpeakEasy/Views/DetailView.swift
@@ -1,144 +1,70 @@
 import SwiftUI
 
 struct DetailView: View {
+    var location: Location
+    
     var body: some View {
         VStack {
-            // Contenu principal dans un ScrollView
             ScrollView {
                 VStack(spacing: 15) {
-                    // Image et titre
-                    Image(systemName: "wineglass")
+                    Image(systemName: location.type.icon)
                         .resizable()
                         .scaledToFit()
-                        .frame(width: 100, height: 100)
-                        .foregroundColor(.white)
-                        .background(Circle().fill(Color.brown).frame(width: 120, height: 120))
+                        .frame(width: 70, height: 70)
+                        .padding()
+                        .foregroundColor(Color("White"))
+                        .background(Circle().fill(Color("Primary")).frame(width: 120, height: 120))
+                        .shadow(radius: 15)
                     
-                    Text("The Glory H")
-                        .font(.title) // Taille de police modérée
+                    Text(location.name)
+                        .font(.title)
                         .fontWeight(.bold)
-                        .foregroundColor(.white)
                     
-                    Text("Bar")
+                    Text(location.type.rawValue.capitalized)
                         .font(.subheadline)
-                        .foregroundColor(.gray)
+                        .opacity(0.5)
                     
                     // Carrousel d'images
                     TabView {
-                        ForEach(1...5, id: \.self) { index in
-                            Image("image\(index)") // Remplacez par vos images dans les Assets
-                                .resizable()
-                                .scaledToFill()
+                        ForEach(location.photos.indices) { index in
+                            AsyncImage(url: URL(string: location.photos[index]))
                                 .frame(height: 200)
+                                .scaledToFill()
                                 .clipped()
                                 .cornerRadius(10)
-                                .padding(.horizontal, 10)
                         }
                     }
-                    .frame(height: 200) // Hauteur modérée du carrousel
-                    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic)) // Style carrousel avec indicateurs
+                    .frame(height: 200)
+                    .clipped()
+                    .cornerRadius(10)
+                    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
                     .padding(.vertical, 15)
-                    
-                    // Ligne avec deux boutons côte à côte
+
                     HStack(spacing: 20) {
-                        // Bouton Appeler
-                        Button(action: {
-                            print("Appeler")
-                        }) {
-                            VStack(spacing: 10) {
-                                Image(systemName: "phone.fill")
-                                    .font(.system(size: 30))
-                                    .foregroundColor(.gray)
-                                Text("Appeler")
-                                    .font(.headline)
-                                    .foregroundColor(.white)
-                            }
-                            .frame(maxWidth: .infinity, minHeight: 90)
-                            .padding()
-                            .background(Color.gray.opacity(0.2))
-                            .cornerRadius(12)
-                        }
+                        GlassedButton(
+                            systemImageName: "phone.fill",
+                            text: "Call"
+                        )
                         
-                        // Bouton Voir le site web
-                        Button(action: {
-                            print("Voir le site web")
-                        }) {
-                            VStack(spacing: 10) {
-                                Image(systemName: "globe")
-                                    .font(.system(size: 30))
-                                    .foregroundColor(.gray)
-                                Text("Voir le site web")
-                                    .font(.headline)
-                                    .foregroundColor(.white)
-                            }
-                            .frame(maxWidth: .infinity, minHeight: 90)
-                            .padding()
-                            .background(Color.gray.opacity(0.2))
-                            .cornerRadius(12)
-                        }
+                        GlassedButton(
+                            systemImageName: "globe",
+                            text: "See the website"
+                        )
                     }
                     
-                    // Bouton "Laisser un commentaire" avec une icône
-                    Button(action: {
-                        print("Laisser un commentaire")
-                    }) {
-                        VStack(spacing: 10) {
-                            Image(systemName: "message.fill")
-                                .font(.system(size: 30))
-                                .foregroundColor(.gray)
-                            Text("Laisser un commentaire")
-                                .font(.headline)
-                                .foregroundColor(.white)
-                        }
-                        .padding()
-                        .frame(maxWidth: .infinity)
-                        .background(Color.gray.opacity(0.2))
-                        .cornerRadius(12)
-                    }
+                    GlassedButton(
+                        systemImageName: "message.fill",
+                        text: "Leave a comment"
+                    )
                 }
                 .padding()
             }
-            
-            // Barre de navigation en bas
-            HStack {
-                Button(action: {
-                    print("Retour à l'accueil")
-                }) {
-                    VStack {
-                        Image(systemName: "house.fill")
-                            .font(.title2)
-                            .foregroundColor(.white)
-                        Text("Accueil")
-                            .font(.footnote)
-                            .foregroundColor(.white)
-                    }
-                    .padding()
-                }
-                .frame(maxWidth: .infinity)
-                
-                Button(action: {
-                    print("Vers la liste (non implémenté)")
-                }) {
-                    VStack {
-                        Image(systemName: "list.bullet")
-                            .font(.title2)
-                            .foregroundColor(.white)
-                        Text("Liste")
-                            .font(.footnote)
-                            .foregroundColor(.white)
-                    }
-                    .padding()
-                }
-                .frame(maxWidth: .infinity)
-            }
-            .background(Color.gray.opacity(0.2))
         }
-        .background(Color.black.edgesIgnoringSafeArea(.all))
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView()
+        DetailView(location: Location.getLyonLocationsWithFullData()[0])
     }
 }
---
+++ SpeakEasy/Views/Graphics/GlassedButton.swift
@@ -0,0 +1,40 @@
+//
+//  GlassedButton.swift
+//  SpeakEasy
+//
+//  Created by Bastien Bombardella on 11/12/2024.
+//
+
+import SwiftUI
+
+struct GlassedButton: View {
+    
+    var systemImageName: String
+    var text: String
+    
+    var body: some View {
+        Button(action: {}) {
+            VStack(spacing: 10) {
+                Image(systemName: systemImageName)
+                    .font(.system(size: 30))
+                    .foregroundColor(Color("Gray").opacity(0.8))
+                Text(text)
+                    .font(.headline)
+                    .foregroundColor(Color.secondary)
+            }
+            .frame(maxWidth: .infinity, minHeight: 90)
+            .padding()
+            .background(Color.gray.opacity(0.2))
+            .cornerRadius(12)
+        }
+    }
+}
+
+struct GlassedButtonPreview: PreviewProvider {
+    static var previews: some View {
+        GlassedButton(
+            systemImageName: "phone.fill",
+            text: "Call"
+        )
+    }
+}
